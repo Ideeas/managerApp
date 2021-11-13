28dca085a6ab7f9ced838c01050cdb4481d9ec3b
Vinicius <viniciusantos.florencio@gmail.com>
2021-11-13 17:18:00 -0300

    adjust:base url

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 import { BASE_URL } from '@env'
 
 export const axios = Axios.create({
-  baseURL: 'https://manage-projects-api.herokuapp.com/',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
