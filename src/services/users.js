21b7526d4ec5b02d5ec397891628bfebf7ddb911
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-05 11:31:33 -0300

    feat:user model and service

---
+++ src/services/users.js
@@ -0,0 +1,69 @@
+import { DBconnection } from '../database'
+
+const table = 'users'
+const db = DBconnection.getConnection()
+
+export default class UserService {
+  static create(user) {
+    return new Promise((resolve, reject) =>
+      db.transaction(
+        (tx) => {
+          tx.executeSql(
+            `insert into ${table} (name, email, username, password) values (?, ?, ?, ?)`,
+            [user.name, user.email, user.username, user.password],
+            (_, { insertId, rows }) => {
+              resolve(insertId)
+            }
+          ),
+            (error) => {
+              console.log('SQL ERROR:' + error)
+            }
+        },
+        (txError) => {
+          console.log(txError)
+        }
+      )
+    )
+  }
+
+  static findOne({ username, password }) {
+    return new Promise((resolve, reject) =>
+      db.transaction(
+        (tx) => {
+          tx.executeSql(
+            `select * from ${table} where column username like '%${username}%' and column password like '%${password}%''`,
+            [],
+            (_, { user, rows }) => {
+              console.log(user)
+              resolve(user)
+            }
+          ),
+            (error) => {
+              console.log('SQL ERROR:' + error)
+            }
+        },
+        (txError) => {
+          console.log(txError)
+        }
+      )
+    )
+  }
+
+  static findAll() {
+    return new Promise((resolve, reject) =>
+      db.transaction(
+        (tx) => {
+          tx.executeSql(`select * from ${table}`, [], (_, { rows }) => {
+            resolve(rows)
+          }),
+            (error) => {
+              console.log('SQL ERROR: ' + error)
+            }
+        },
+        (txError) => {
+          console.log(txError)
+        }
+      )
+    )
+  }
+}
