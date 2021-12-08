f064f3f405c978224746f82e7a2ac7a6be3842e6
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-08 09:37:54 -0300

    feat:verify if user has been already auth

--- src/general/redux/user/index.js
+++ src/general/redux/user/index.js
@@ -1,14 +1,29 @@
+export const HAS_USER_AUTH = 'HAS_USER_AUTH'
+export const USER_AUTHENTICATED = 'USER_AUTHENTICATED'
 export const USER_AUTH_SUCCESS = 'USER_AUTH_SUCCESS'
 export const USER_AUTH_FAIILED = 'USER_AUTH_FAIILED'
 export const AUTH_USER = 'AUTH_USER'
 
 const userReducer = (state, action) => {
+  if (action.type === USER_AUTHENTICATED) {
+    return { ...state, user: action.payload }
+  }
+
   if (action.type === USER_AUTH_SUCCESS) {
     return { ...state, user: action.payload }
   }
 
   if (action.type === USER_AUTH_FAIILED) {
     return { ...state, error: action.payload }
+  }
+}
+
+export const hasUserAuth = () => {
+  return {
+    type: HAS_USER_AUTH,
+    meta: {
+      reducer: userReducer,
+    },
   }
 }
 
