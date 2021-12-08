d415d31bc632c99edfea017cb8a5093d20f30495
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-08 16:40:13 -0300

    feat:global button status component

--- src/views/Home/components/renderProjects.js
+++ src/views/Home/components/renderProjects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native'
 import { COLORS } from '../../../general/styles/colors'
-import ButtonStatus from './ButtonStatus'
+import ButtonStatus from '../../../general/components/ButtonStatus'
 
 const renderProjects = (props) => {
   const { cardData, isInteracting, handlePressIn, handlePressOut } = props
